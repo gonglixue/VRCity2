66b2eaa15caf77bb877a982f2940172aa9a3788b
gonglixue <724351282@qq.com>
2017-03-20 18:10:47 +0800

    vis screen

---
+++ Assets/Scripts/vis/monthDataController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monthDataController : MonoBehaviour {
+    const float maxHeight = 7;
+    public int[] monthData;
+
+    public GameObject linePrefab;
+	// Use this for initialization
+	void Start () {
+        
+        // find max
+        int max = monthData[0];
+        for(int j = 1; j < monthData.Length; j++)
+        {
+            if (monthData[j] > max)
+                max = monthData[j];
+        }
+
+        // 设置高度
+        int i = 0;
+        
+        foreach (Transform child in this.transform)
+        {
+            if (i > 11)
+                break;
+
+            Debug.Log(child.name);
+            child.GetComponent<monthDotInfo>().setValue(monthData[i], max);
+            
+
+            // TODO 
+            // 连线
+            if(i > 0)
+            {
+                Transform lastChild = this.transform.GetChild(i - 1);
+                float distance = Vector3.Distance(lastChild.position, child.position);
+                Vector3 centerPos = (lastChild.position + child.position) / 2.0f;
+                GameObject line = Instantiate(linePrefab, centerPos, Quaternion.identity, this.transform) as GameObject;
+                line.transform.forward = child.position - lastChild.position;
+                line.transform.localScale = new Vector3(line.transform.localScale.x, line.transform.localScale.y, distance);
+            }
+
+            i++;
+        }
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/vis/monthDotInfo.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monthDotInfo : MonoBehaviour {
+
+    public int value;
+    public int month;  // 1-12
+
+    static float maxHeight = 7.0f;
+
+    public GameObject verticalBar;
+    public GameObject line;
+
+    void Awake()
+    {
+        createVerticalBar();
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setValue(int v, int max)
+    {
+        value = v;
+        float newPosY = v*1.0f / max * maxHeight;
+
+        // TODO
+        // ease transformation
+        this.transform.position = new Vector3(transform.position.x, newPosY, transform.position.z);
+    }
+
+    void clickThisMonth()
+    {
+        // 点击查看本月详细信息
+
+    }
+
+    void createVerticalBar()
+    {
+        float vBary = 4.0f;
+        Vector3 vBarPos = new Vector3(this.transform.position.x, vBary, this.transform.position.z);
+        GameObject vBar = Instantiate(verticalBar) as GameObject;
+        
+        vBar.transform.position = vBarPos;
+        vBar.transform.SetParent(this.transform.parent);
+
+
+
+    }
+}
