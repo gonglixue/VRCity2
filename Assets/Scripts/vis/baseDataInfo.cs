69704183f731cde6b7306875e6dc3a2be866f946
gonglixue <724351282@qq.com>
2017-03-23 18:14:34 +0800

    add base

---
+++ Assets/Scripts/vis/baseDataInfo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baseDataInfo : MonoBehaviour {
+    public Vector3 localPosition = new Vector3(0, 0, 0);
+    public Vector3 localScale = new Vector3(0.7f, 10, 0.7f);
+
+    static float maxHeight = 25f;
+
+    private int _value;
+    public GameObject cylinderPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void InitData(int data)
+    {
+        _value = data;
+        CreateCylinder(data);
+    }
+
+    void CreateCylinder(int data)
+    {
+        Vector3 cylinderLocalPos = this.transform.parent.localToWorldMatrix.MultiplyPoint(localPosition);
+        GameObject cylinder = Instantiate(cylinderPrefab, cylinderLocalPos, Quaternion.identity, this.transform) as GameObject;
+        float scaleY = maxHeight / this.transform.parent.GetComponent<classGroupController>().maxBaseData * data;
+        cylinder.transform.localScale = new Vector3(localScale.x, scaleY, localScale.z);
+    }
+}
---
+++ Assets/Scripts/vis/classGroupController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class classGroupController : MonoBehaviour {
+    public float maxBaseData;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
