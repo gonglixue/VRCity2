3e6eb24a5708a510f4deab1a98b1439aaab2cb61
gonglixue <724351282@qq.com>
2017-03-24 20:27:45 +0800

    skybox config

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -15,6 +15,8 @@ public class WeatherController : MonoBehaviour {
     public GameObject sunnySystem;
     public GameObject cloudySystem;
 
+    public GameObject skyBox;
+
 	// Use this for initialization
 	void Start () {
         ChooseWeather();
@@ -27,18 +29,27 @@ public class WeatherController : MonoBehaviour {
 
     void InitSnowySystem()
     {
+        SkyBoxConfig snowSkyConf = new SkyBoxConfig(new Color(3, 30, 49, 255), new Color(75, 86, 86, 255), new Color(167, 168, 168), 0.3f, 0.079f, 0.821f);
+        SetSkyBoxAttrib(snowSkyConf);
+
         snowySystem.SetActive(true);  // 开启粒子系统
     }
     void InitRainySystem()
     {
+        SkyBoxConfig rainSkyConf = new SkyBoxConfig(new Color(3, 30, 49, 255), new Color(75, 86, 86, 255), new Color(167, 168, 168), 0.3f, 0.079f, 0.821f);
+        SetSkyBoxAttrib(rainSkyConf);
         rainySystem.SetActive(true);
     }
     void InitSunnySystem()
     {
+        SkyBoxConfig sunnySkyConf = new SkyBoxConfig(new Color(20, 124, 197, 255), new Color(181, 216, 246, 255), new Color(255, 255, 255), 0.3f, 0.0f, 1.0f);
+        SetSkyBoxAttrib(sunnySkyConf);
         sunnySystem.SetActive(true);
     }
     void InitCloudySystem()
     {
+        SkyBoxConfig cloudSkyConf = new SkyBoxConfig(new Color(20, 124, 197, 255), new Color(181, 216, 246, 255), new Color(191, 191, 191), 0.3f, 0.0f, 0.32f);
+        SetSkyBoxAttrib(cloudSkyConf);
         cloudySystem.SetActive(true);
     }
 
@@ -51,5 +62,38 @@ public class WeatherController : MonoBehaviour {
             case WeatherMode.Snowy: InitSnowySystem(); break;
             case WeatherMode.Sunny: InitSunnySystem(); break;
         }
+    }
+
+    // 根据天气设置天空盒参数
+    void SetSkyBoxAttrib(SkyBoxConfig config)
+    {
+        Material m = skyBox.GetComponent<MeshRenderer>().material;
+        m.SetColor("_SkyColor0", config.colorAbove);
+        m.SetColor("_SkyColor1", config.colorBelow);
+        m.SetColor("_CloudColor", config.cloudColor);
+        m.SetFloat("_Speed", config.speed);
+        m.SetFloat("_Emptiness", config.emptiness);
+        m.SetFloat("_Sharpness", config.sharpness);
+
+    }
+}
+
+public class SkyBoxConfig
+{
+    public Color colorAbove;
+    public Color colorBelow;
+    public Color cloudColor;
+    public float speed;
+    public float emptiness;
+    public float sharpness;
+
+    public SkyBoxConfig(Color _colorAbove, Color _colorBelow, Color _cloudColor, float _speed, float _emptiness, float _sharpness)
+    {
+        colorAbove = _colorAbove;
+        colorBelow = _colorBelow;
+        cloudColor = _cloudColor;
+        speed = _speed;
+        emptiness = _emptiness;
+        sharpness = _sharpness;
     }
 }
