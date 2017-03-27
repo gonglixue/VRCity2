78e616826e5b5fc5037d758632541c825c348e6e
gonglixue <724351282@qq.com>
2017-03-27 20:22:21 +0800

    basic www method

---
+++ Assets/Scripts/NetworkService.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NetworkService{
+
+    //private const string xmlApi = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&mode=xml";
+    private const string xmlApi = "http://127.0.0.1:8888/upload";
+
+
+    private bool IsResponseValid(WWW www)
+    {
+        if(www.error != null)
+        {
+            Debug.Log("bad connection");
+            Debug.Log(www.error);
+            return false;
+        }
+        else if(string.IsNullOrEmpty(www.text))
+        {
+            Debug.Log("bad data");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private IEnumerator CallAPI(string url, Action<string> callback)
+    {
+        WWW www = new WWW(url);
+        yield return www;
+
+        if(!IsResponseValid(www))
+        {
+            yield break;
+        }
+
+        callback(www.text);
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        Debug.Log("get wheather xml");
+        return CallAPI(xmlApi, callback);
+    }
+}
---
+++ Assets/Scripts/test/wwwTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wwwTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NetworkService service = new NetworkService();
+        Startup(service);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Startup(NetworkService service)
+    {
+        StartCoroutine(service.GetWeatherXML(OnXMLDataLoaded));
+    }
+
+    public void OnXMLDataLoaded(string data)
+    {
+        Debug.Log(data);
+        
+    }
+}
